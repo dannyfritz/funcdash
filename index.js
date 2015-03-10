81ac6ff365d29feaa29d999e724bfc4e82eacccc
Danny Fritz <dannyfritz@gmail.com>
2015-03-10 13:23:32 -0500

    :bug: export all the functions.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = {
+	and: require('./lib/and'),
+	apply: require('./lib/apply'),
 	multiply: require('./lib/multiply'),
+	not: require('./lib/not'),
+	or: require('./lib/or'),
 	product: require('./lib/product')
 };
