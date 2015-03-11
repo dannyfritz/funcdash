3327fee141c633959739868372f1ad0976db5081
Danny Fritz <dannyfritz@gmail.com>
2015-03-11 10:47:15 -0500

    :bug: Add missing methods to index.js.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
 	and: require('./lib/and'),
 	apply: require('./lib/apply'),
+	call: require('./lib/call'),
+	mapInvoke: require('./lib/mapInvoke'),
 	multiply: require('./lib/multiply'),
 	not: require('./lib/not'),
 	or: require('./lib/or'),
---
+++ test/mixin.js
@@ -0,0 +1,15 @@
+var _ = require('lodash').runInContext();
+var test = require('tape');
+_.mixin(require('..'));
+
+test('and', function (t) {
+	t.equal(_.and(true, true), true);
+	t.equal(_.apply(_.add, [1, 2]), 3);
+	t.equal(_.call(_.add, 1, 2), 3);
+	t.deepEqual(_.mapInvoke(_.add, _.multiply)(1, 2), [3, 2]);
+	t.equal(_.multiply(2, 3), 6);
+	t.equal(_.not(false), true);
+	t.equal(_.or(false, true), true);
+	t.equal(_.product([1, 2, 3]), 6);
+	t.end();
+});
